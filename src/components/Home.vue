9ad9f8d1440afa6aa7ac8b28cccfe1d7fbdc3930
Hnin Hnin Aye <hninhninaye11691@gmail.com>
2020-04-26 23:18:39 +0900

    [WIP] add 「todo」impl:

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,23 +1,95 @@
 <template>
   <div>
-    <v-toolbar dark color="teal">
-      <button @click.prevent="logout">Logout</button>
+    <v-toolbar color="cyan" dark>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title> TODO LIST </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn @click.prevent="logout"> Logout </v-btn>
     </v-toolbar>
-    <v-container>
-      //implement todo list
-    </v-container>
+    <v-card class="mx-auto" max-width="500">
+      <v-list>
+        <v-subheader>Today</v-subheader>
+        <v-list-item v-if="filteredTasks.length == 0">
+          <v-list-item-content>
+            <v-list-item-title><b> Nothing to do. Hurrah!</b></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item-group v-for="t in filteredTasks" v-bind:key="t.action" v-else>
+          <v-list-item>
+            <v-list-item-content>
+              {{ t.action }}
+            </v-list-item-content>
+            <v-list-item-action>
+              <v-checkbox
+                v-model="t.done"
+                color="primary"
+              >
+              </v-checkbox>
+            </v-list-item-action>
+          </v-list-item>
+        </v-list-item-group>
+
+        <v-list-item>
+          <v-text-field label="Add Task" v-model="newItemText"></v-text-field>
+          <v-btn small color="primary" dark @click="addNewTodo">Add</v-btn>
+        </v-list-item>
+
+        <v-divider :inset="inset"></v-divider>
+        <v-list-item>
+          <v-list-item-action>
+            <v-checkbox
+              v-model="hideCompleted"
+              color="primary"
+            >
+            </v-checkbox>
+          </v-list-item-action>
+          <v-list-item-content>
+            Hide completed tasks
+          </v-list-item-content>
+
+        </v-list-item>
+      </v-list>
+    </v-card>
+
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      tasks: [],
+      hideCompleted: true,
+      newItemText: ''
+    };
+  },
+  computed: {
+    filteredTasks() {
+      return this.hideCompleted ? this.tasks.filter(t => !t.done) : this.tasks
+    }
   },
   methods: {
     async logout () {
       await this.$auth.logout()
       this.$router.push('/')
+    },
+    // add new task
+    addNewTodo() {
+      if(!this.newItemText) return;
+      this.tasks.push({
+        action: this.newItemText,
+        done: false
+        });
+      // stored as json data
+      localStorage.setItem('todos', JSON.stringify(this.tasks));
+      this.newItemText = '';
+    }
+  },
+  // get Json data
+  created() {
+    let data = localStorage.getItem("todos");
+    if (data != null) {
+      this.tasks = JSON.parse(data);
     }
   }
 }
